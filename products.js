ba7dc654fa69e485627f1cb1a5cfa542cb551ece
Mikael-Rt-JS <61776022+Mikael-Rt-JS@users.noreply.github.com>
2024-04-21 15:05:38 +0400

    Create products.js

---
+++ products.js
@@ -0,0 +1,98 @@
+let data_products=[
+  {
+    id:'1',
+    title: 'Pagani Huayra R Evo',
+    images: 'https://cdn-m.sport24.ru/m/c0bc/2cb1/ad73/4107/a9e5/b97e/fffe/c4ff/1600_10000_max.jpeg',
+    promotion: '',
+    raiting_1: 0,
+    raiting_2: 0,
+    raiting_3: 0,
+    raiting_4: 0,
+    raiting_5: 0,
+    price: '8230'
+  },
+  {
+    id:'2',
+    title: 'Lamborghini Aventador',
+    images: 'https://avatars.mds.yandex.net/get-vertis-journal/3911415/636085.jpg_1675685003302/orig',
+    promotion: '',
+    raiting_1: 0,
+    raiting_2: 0,
+    raiting_3: 0,
+    raiting_4: 0,
+    raiting_5: 0,
+    price: '10300'
+  },
+  {
+    id:'3',
+    title: 'Sinister Rolls-Royce',
+    images: 'https://cdn.motor1.com/images/mgl/13jgw/s3/rolls-royce-black-badge-wraith-by-spofec.jpg',
+    promotion: '',
+    raiting_1: 0,
+    raiting_2: 0,
+    raiting_3: 0,
+    raiting_4: 0,
+    raiting_5: 0,
+    price: '126300'
+  },
+  {
+    id:'4',
+    title: 'New Ford Ranger Raptor',
+    images: 'https://editorial.pxcrush.net/constructionsales/general/editorial/ford-ranger-raptor-render-01-nwtf.jpg?width=1024&height=683',
+    promotion: '',
+    raiting_1: 0,
+    raiting_2: 0,
+    raiting_3: 0,
+    raiting_4: 0,
+    raiting_5: 0,
+    price: '34300'
+  },
+  {
+    id:'5',
+    title: '2020 Bugatti Chiron Review',
+    images: 'https://newsroomcdn.bugatti.com/w_1800,q_75/276lo0iu03p0-01-bugatti-css-golden-era-making-of.webp',
+    promotion: '',
+    raiting_1: 0,
+    raiting_2: 0,
+    raiting_3: 23,
+    raiting_4: 0,
+    raiting_5: 40,
+    price: '300000'
+  },
+  {
+    id:'6',
+    title: 'Volvo launches',
+    images: 'https://www.locate2u.com/wp-content/uploads/venn-1.webp',
+    promotion: '',
+    raiting_1: 5,
+    raiting_2: 0,
+    raiting_3: 0,
+    raiting_4: 300,
+    raiting_5: 0,
+    price: '420000'
+  },
+  {
+    id:'7',
+    title: 'Alstom\'s Avelia Horizon',
+    images: 'https://www.tencom.com/hs-fs/hubfs/AdobeStock_564803106-1.jpeg?width=900&height=600&name=AdobeStock_564803106-1.jpeg',
+    promotion: '',
+    raiting_1: 0,
+    raiting_2: 0,
+    raiting_3: 1,
+    raiting_4: 0,
+    raiting_5: 0,
+    price: '864000'
+  },
+  {
+    id:'8',
+    title: 'Alstom\'s Avelia Horizon',
+    images: 'https://zolimacitymag.com/wp-content/uploads/2024/01/Technical-University-of-Munich-vision-hyperloop-high-speed-train-zolima-citymag.jpg',
+    promotion: '',
+    raiting_1: 0,
+    raiting_2: 0,
+    raiting_3: 0,
+    raiting_4: 0,
+    raiting_5: 0,
+    price: '1200000'
+  }
+];
